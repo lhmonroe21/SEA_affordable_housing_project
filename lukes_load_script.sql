c868df5c3f78ab76a199e1e3a51f0d867d87771b
LUKE MONROE <modestlyawesome@LUKEs-MacBook-Pro.local>
2019-03-23 12:04:53 -0700

    First Readme Commit

---
+++ lukes_load_script.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS sea_aff_housing;
+
+CREATE TABLE IF NOT EXISTS sea_aff_housing.seattle_income_restricted_housing (
+	property_name TEXT,
+	property_address TEXT,
+	zip_code INT,
+	num_restricted_units TEXT,
+	hud TEXT,
+	sha TEXT,
+	state_of_wa_or_county TEXT,
+	wshfc TEXT,
+	city_seattle TEXT,
+	year_placed_in_service INT,
+	council_distric TEXT
+	); 
+	
+DELETE FROM sea_aff_housing.seattle_income_restricted_housing;
+
+COPY sea_aff_housing.seattle_income_restricted_housing FROM '/Users/modestlyawesome/Downloads/Galvanize/rent_and_income_restricted_housing.csv'
+WITH DELIMITER ',' CSV HEADER;
+
+COMMIT;
+
+SELECT sum(num_restricted_units::int), council_distric
+FROM sea_aff_housing.seattle_income_restricted_housing
+GROUP BY council_distric
+ORDER BY council_distric
